a440cfc3fdce6daf5929f655cf3fe3caa41bc00f
tramhuuloi <tramhuuloi123@gmail.com>
2024-01-08 15:09:06 +0700

    add doan

---
+++ scr/doanchuyennganh/doanchuyennganh/doanchuyennganh/map.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace doanchuyennganh
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class map : ContentPage
+	{
+		public map ()
+		{
+			InitializeComponent ();
+		}
+        public void SetPhonghoc(phonghoc phong)
+        {
+            BindingContext = phong;
+        }
+    }
+}
---
+++ scr/doanchuyennganh/doanchuyennganh/doanchuyennganh/thongtintracuu.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace doanchuyennganh
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class thongtintracuu : ContentPage
+	{
+		public thongtintracuu ()
+		{
+			InitializeComponent ();
+        }
+
+        public void SetPhonghoc(phonghoc phong)
+        {
+            BindingContext = phong;
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            if (Application.Current.Properties.ContainsKey("SelectedPhong"))
+            {
+                // Lấy thông tin phòng đã chọn
+                phonghoc selectedPhong = Application.Current.Properties["SelectedPhong"] as phonghoc;
+
+                map mapPage = new map();
+
+                // Gán dữ liệu
+                mapPage.SetPhonghoc(selectedPhong);
+
+                // Chuyển hướng
+                await Navigation.PushAsync(mapPage);
+            }
+            else
+            {
+                await DisplayAlert("Thông báo", "Vui lòng tra cứu phòng trước khi xem thông tin", "OK");
+            }
+            
+        }
+    }
+}
